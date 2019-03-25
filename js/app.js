5c61fcf5f9f29d66a43f343834ba46dcb4440f52
keenmaster486 <cebrock106@gmail.com>
2019-03-25 15:12:53 -0600

    Initial commit: part 1

---
+++ js/app.js
@@ -0,0 +1,80 @@
+console.log("app.js loaded");
+
+//1. Data types
+
+// A light switch that can be either on or off.
+
+//use a boolean
+let lightswitch = true; //true represents "on"
+let switches = [true, false, false, true]; //etc etc etc
+
+
+// A user's email address.
+
+//use a string
+let email = "drbeep123@yahoo.com";
+let useremails = ["email@website.com", "email2@website.com", "email3@website2.com"]; //etc
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+
+let spaceship = {
+	hull: "stainless steel",
+	blasters: 2,
+	tractorbeam: 1,
+	warpdrive: "dilithium"
+};
+
+// A list of student names from our class.
+
+//Array of strings
+let studentnames = ["Bob", "Sue", "Jim", "Page"];
+
+// A list of student names from our class, each with a location.
+let students1 = [
+	{
+		name: "Bob",
+		location: "Chicago"
+	},
+	{
+		name: "Sue",
+		location: "Los Angeles"
+	},
+	{
+		name: "Jim",
+		location: "Denver"
+	},
+	{
+		name: "Page",
+		location: "Boston"
+	}
+];
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+let students2 = [
+	{
+		name: "Bob",
+		location: "Chicago",
+		tvshows: ["Seinfeld", "The Office"]
+	},
+	{
+		name: "Sue",
+		location: "Los Angeles",
+		tvshows: ["Brooklyn 99", "Parks and Rec"]
+	},
+	{
+		name: "Jim",
+		location: "Denver",
+		tvshows: ["Friends", "How I met your mother"]
+	},
+	{
+		name: "Page",
+		location: "Boston",
+		tvshows: ["Star Trek TNG", "Mythbusters"]
+	}
+];
+
+
+
+
+
+
